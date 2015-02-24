c5926d601c67c70ad0dacb8fbdc63b1c50cdbaea
Tosha Antokhin <tosha.antokhin@gmail.com>
2015-02-24 14:48:04 -0600

    Updates to battleship game

--- W1D2/BattleShip/src/battleship/BattleShip.java
+++ W1D2/BattleShip/src/battleship/BattleShip.java
@@ -5,43 +5,59 @@
  */
 package battleship;
 
+import java.io.*;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  *
  * @author AAntokhin
  */
 public class BattleShip {
-    private int[][] matrix = new int[5][5]; //matrix holding state of all cells, x and y
+
+    private int[][] matrix = new int[5][5]; //matrix holding state of all cells
     private int[][] userMatrix = new int[5][5]; //matrix holding state of all revealed cells
-    private int[][] shipLocations = new int[2][2]; //matrix holding locations for ship cells
+    private int[][] shipLocations = new int[3][2]; //matrix holding locations for ship cells
+    private int numShipCells;
     private boolean isAlive;
-    
-    
-    private int getCellState(int x, int y) {
-        //1 = no ship, 2 = ship
-        return this.matrix[x][y];
-    }
-    
+
     public boolean getIsAlive() {
         return isAlive;
     }
-    
-    public int getGameCellState(int x, int y) {
+
+    public int getCellState(int x, int y) {
         if (this.userMatrix[x][y] == 0) {
             return 0;
         } else {
-            return this.getCellState(x,y);
+            //1 = no ship, 2 = ship
+            return this.matrix[x][y];
         }
     }
-    
+
     private void setCellState(int x, int y, int state) {
         this.matrix[x][y] = state;
     }
-        
+
     public int getMatrixSize() {
         return this.matrix.length;
     }
-    
+
     private void setUpCells() {
+        Path inFile = Paths.get(System.getProperty("user.dir") + "\\src\\battleship\\gamesetup.txt");
+
+        Charset charset = Charset.forName("US-ASCII");
+        try (BufferedReader reader = Files.newBufferedReader(inFile, charset)) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+
         for (int i = 0; i < this.matrix.length; i++) {
             for (int j = 0; j < this.matrix[i].length; j++) {
                 this.matrix[i][j] = 1;
@@ -49,33 +65,52 @@ public class BattleShip {
                 for (int[] ship : this.shipLocations) {
                     if (ship[0] == i && ship[1] == j) { //if coords match, set ship
                         this.matrix[i][j] = 2;
-                    } 
+                    }
                 }
             }
         }
     }
-    
+
     public void startGame() {
         this.shipLocations[0][0] = 0;
         this.shipLocations[0][1] = 0;
-        
+
         this.shipLocations[1][0] = 0;
         this.shipLocations[1][1] = 1;
-        
+
+        this.shipLocations[2][0] = 0;
+        this.shipLocations[2][1] = 2;
+
         this.isAlive = true;
         setUpCells();
-        
+
     }
-    
+
     public int tryHit(int x, int y) {
-        this.userMatrix[x][y] = 1;
+        if (this.userMatrix[x][y] == 1) {
+            return 2;
+        } else {
+            this.userMatrix[x][y] = 1;
+        }
+
         if (this.matrix[x][y] == 2) {
             return 1;
         }
         return 0;
     }
-    
+
     public void update() {
-        
+        int hits = 0;
+        for (int i = 0; i < this.matrix.length; i++) { //iterate over arrays to check total hits
+            for (int j = 0; j < this.matrix[i].length; j++) {
+                if (matrix[i][j] == 2 && userMatrix[i][j] == 1) {
+                    hits++; //add hit to total
+                }
+            }
+        }
+
+        if (hits >= this.shipLocations.length) { //if # of hits is greater than # of ship cells, end game
+            this.isAlive = false;
+        }
     }
 }
--- W1D2/BattleShip/src/battleship/Launcher.java
+++ W1D2/BattleShip/src/battleship/Launcher.java
@@ -18,20 +18,36 @@ public class Launcher {
         
         game.startGame();
         
-        while(game.getIsAlive()) {
+        while(game.getIsAlive() == true) {
+            System.out.println(" 12345");
             for (int i=0; i < game.getMatrixSize(); i++) { //print game display
-                String output = "";
+                String output = "" + (i + 1); //add 1 to row numbers for readability
                 for (int j=0; j < game.getMatrixSize(); j++) {
-                    output = output + game.getGameCellState(i, j);
+                    if (game.getCellState(i, j) == 0) {
+                        output = output + "|"; // not a hit or a miss
+                    } else if (game.getCellState(i, j) == 1) {
+                        output = output + "X"; // previous miss
+                    } else if (game.getCellState(i, j) == 2) {
+                        output = output + "O"; // previous hit
+                    }
+                
                 }
                 System.out.println(output);
             }
-            int x = Integer.parseInt(helper.GetUserInput("Your x guess:"));
-            int y = Integer.parseInt(helper.GetUserInput("Your y guess:"));
-            int result = game.tryHit(x, y);
+            
+            //parse user input for 
+            int x = Integer.parseInt(helper.GetUserInput("Enter row number:"));
+            int y = Integer.parseInt(helper.GetUserInput("Enter column number:"));
+            int result = game.tryHit(x - 1, y - 1); //subtract 1 from row and column numbers to account for readability
+            
             if (result == 1) System.out.println("Hit!");
             else if (result == 0) System.out.println("Miss!");
+            else if (result == 2) System.out.println("You already tried this square!");
+            
+            game.update(); //update game to check if we've won
         }
+        
+        System.out.println("Congratulations, you win!");
     }
     
 }
