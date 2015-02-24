134223e21b949f6beb1d9057748ba8be79f62388
Tosha Antokhin <tosha.antokhin@gmail.com>
2015-02-24 09:08:26 -0600

    Updated PhraseOMatic

--- W1D1/PhraseOMatic.java
+++ W1D1/PhraseOMatic.java
@@ -1,7 +1,19 @@
-public PhraseOMatic {
+public class PhraseOMatic {
 	public static void main (String[] args) {
-		System.out.print("Hello World!");
+
+		String[] wordOne = {"24/7", "multi-tier", "30,000 foot", "B-to-B",
+		"win-win", "front-end", "web-based", "pervasive", "smart"};
+		String[] wordTwo = {"empowered", "sticky", "value-added", "oriented",
+		"centric", "distributed", "clustered", "branded", "outside-the-box"};
+		String[] wordThree = {"process", "tipping-point", "strategy", "mindshare",
+		"portal", "space", "vision", "paradigm", "mission"};
+
+		int rand1 = (int) (Math.random() * wordOne.length);
+		int rand2 = (int) (Math.random() * wordTwo.length);
+		int rand3 = (int) (Math.random() * wordThree.length);
+
+		String phrase = wordOne[rand1] + " " + wordTwo[rand2] + " " + wordThree[rand3];
+
+		System.out.println("What we need is a " + phrase);
 	}
 }
-
-//https://zimslifeintcs.files.wordpress.com/2011/12/head-first-java-2nd-edition.pdf
