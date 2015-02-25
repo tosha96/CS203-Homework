eac670326dbc67a1eb7fa1c61872a9550061fc77
Tosha Antokhin <tosha.antokhin@gmail.com>
2015-02-25 09:28:58 -0600

    Added game import, guesses, and game draw function

--- W1D2/BattleShip/src/battleship/BattleShip.java
+++ W1D2/BattleShip/src/battleship/BattleShip.java
@@ -6,7 +6,6 @@
 package battleship;
 
 import java.io.*;
-import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,8 +19,8 @@ public class BattleShip {
 
     private int[][] matrix = new int[5][5]; //matrix holding state of all cells
     private int[][] userMatrix = new int[5][5]; //matrix holding state of all revealed cells
-    private int[][] shipLocations = new int[3][2]; //matrix holding locations for ship cells
     private int numShipCells;
+    private int guesses = 0;
     private boolean isAlive;
 
     public boolean getIsAlive() {
@@ -37,6 +36,10 @@ public class BattleShip {
         }
     }
 
+    public int getGuesses() {
+        return this.guesses;
+    }   
+    
     private void setCellState(int x, int y, int state) {
         this.matrix[x][y] = state;
     }
@@ -51,42 +54,37 @@ public class BattleShip {
         Charset charset = Charset.forName("US-ASCII");
         try (BufferedReader reader = Files.newBufferedReader(inFile, charset)) {
             String line = null;
+            int lineNum = 0;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                for (int i=0;i<line.length(); i++) {
+                    char[] lineArray = line.toCharArray();
+                    this.matrix[lineNum][i] = Character.getNumericValue(lineArray[i]);
+                    if (Character.getNumericValue(lineArray[i]) == 2) {
+                        //in file, 2 = ship, 1 = nothing
+                        this.numShipCells++;
+                    }
+                }
+                lineNum++;
             }
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);
         }
 
-        for (int i = 0; i < this.matrix.length; i++) {
-            for (int j = 0; j < this.matrix[i].length; j++) {
-                this.matrix[i][j] = 1;
+        for (int i = 0; i < this.userMatrix.length; i++) { //loop to initialize all cells as hidden
+            for (int j = 0; j < this.userMatrix[i].length; j++) {
                 this.userMatrix[i][j] = 0;
-                for (int[] ship : this.shipLocations) {
-                    if (ship[0] == i && ship[1] == j) { //if coords match, set ship
-                        this.matrix[i][j] = 2;
-                    }
-                }
             }
         }
     }
 
     public void startGame() {
-        this.shipLocations[0][0] = 0;
-        this.shipLocations[0][1] = 0;
-
-        this.shipLocations[1][0] = 0;
-        this.shipLocations[1][1] = 1;
-
-        this.shipLocations[2][0] = 0;
-        this.shipLocations[2][1] = 2;
-
         this.isAlive = true;
         setUpCells();
 
     }
 
     public int tryHit(int x, int y) {
+        this.guesses++;
         if (this.userMatrix[x][y] == 1) {
             return 2;
         } else {
@@ -109,7 +107,7 @@ public class BattleShip {
             }
         }
 
-        if (hits >= this.shipLocations.length) { //if # of hits is greater than # of ship cells, end game
+        if (hits >= this.numShipCells) { //if # of hits is greater than # of ship cells, end game
             this.isAlive = false;
         }
     }
