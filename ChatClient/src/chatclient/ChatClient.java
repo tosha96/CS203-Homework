cc10e97eabf81305b151098b3239c0826d01bb5c
Tosha Antokhin <tosha.antokhin@gmail.com>
2015-03-06 08:57:03 -0600

    Chat now working

--- ChatClient/src/chatclient/ChatClient.java
+++ ChatClient/src/chatclient/ChatClient.java
@@ -35,8 +35,64 @@ public class ChatClient {
     public void go() {
         JFrame frame = new JFrame("Chat Client");
         JPanel mainPanel = new JPanel();
+        JPanel inputPanel = new JPanel();
         incoming = new JTextArea(15,10);
+        incoming.setLineWrap(true);
+        incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendButtonListener());
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(qScroller);
+        inputPanel.add(outgoing);
+        inputPanel.add(sendButton);
+        mainPanel.add(inputPanel);
         
+        setUpNetworking();
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.setSize(800,500);
+        frame.setVisible(true);
+    }
+    
+    private void setUpNetworking() {
+        try {
+            sock = new Socket("127.0.0.1", 5000);
+            InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+            reader = new BufferedReader(streamReader);
+            writer = new PrintWriter(sock.getOutputStream());
+            System.out.println("Networking established.");
+        } catch (IOException ex)  { ex.printStackTrace(); }
+        
+    }
+        
+    public class SendButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent ev) {
+            try {
+                writer.println(outgoing.getText());
+                writer.flush();
+            } catch (Exception ex)  { ex.printStackTrace(); }
+            outgoing.setText("");
+            outgoing.requestFocus();
+        }
+    }
+    
+    public class IncomingReader implements Runnable {
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (Exception ex)  { ex.printStackTrace(); }
+        }
     }
     
 }
