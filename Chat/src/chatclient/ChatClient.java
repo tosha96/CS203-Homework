be9de95bf658049072ce91f71920f75fb161c4e4
Tosha Antokhin <tosha.antokhin@gmail.com>
2015-03-10 12:10:21 -0500

    Added user list and more system messages

--- Chat/src/chatclient/ChatClient.java
+++ Chat/src/chatclient/ChatClient.java
@@ -20,12 +20,15 @@ import java.util.*;
 public class ChatClient {
 
     JTextArea incoming;
+    JList userList;
     JTextField outgoing;
     JTextField setName;
     ObjectInputStream reader;
     ObjectOutputStream writer;
     Socket sock;
-    
+    Vector<String> users = new Vector<>();
+    DefaultListModel usersModel = new DefaultListModel();
+
     /**
      * @param args the command line arguments
      */
@@ -33,76 +36,101 @@ public class ChatClient {
         ChatClient client = new ChatClient();
         client.go();
     }
-    
+
     public void go() {
         JFrame frame = new JFrame("Chat Client");
         JPanel mainPanel = new JPanel();
         JPanel inputPanel = new JPanel();
         JPanel namePanel = new JPanel();
-        incoming = new JTextArea(15,10);
+
+        userList = new JList(usersModel);
+        userList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        userList.setLayoutOrientation(JList.VERTICAL);
+        userList.setVisibleRowCount(-1);
+
+        JScrollPane userScroller = new JScrollPane(userList);
+        userScroller.setPreferredSize(new Dimension(100, 500));
+
+        incoming = new JTextArea(15, 10);
         incoming.setLineWrap(true);
         incoming.setEditable(false);
+
         JScrollPane qScroller = new JScrollPane(incoming);
         qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
         outgoing = new JTextField(20);
-        setName = new JTextField(20);
+        setName = new JTextField(10);
         JButton sendButton = new JButton("Send");
         JButton setNameButton = new JButton("Set Name");
         sendButton.addActionListener(new SendButtonListener());
         setNameButton.addActionListener(new SetNameButtonListener());
+
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         mainPanel.add(qScroller);
-        inputPanel.add(outgoing);
-        inputPanel.add(sendButton);
+
         namePanel.add(setName);
         namePanel.add(setNameButton);
-        mainPanel.add(inputPanel);
-        mainPanel.add(namePanel);
         
+        inputPanel.add(namePanel);
+        inputPanel.add(outgoing);
+        inputPanel.add(sendButton);
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+        frame.getContentPane().add(BorderLayout.EAST, userScroller);
+        frame.getContentPane().add(BorderLayout.SOUTH, inputPanel);
+
         setUpNetworking();
         Thread readerThread = new Thread(new IncomingReader());
         readerThread.start();
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
-        frame.setSize(800,500);
+        frame.setSize(800, 500);
         frame.setVisible(true);
     }
-    
+
     private void setUpNetworking() {
         try {
             sock = new Socket("127.0.0.1", 5000);
             reader = new ObjectInputStream(sock.getInputStream());
             writer = new ObjectOutputStream(sock.getOutputStream());
             System.out.println("Networking established.");
-        } catch (IOException ex)  { ex.printStackTrace(); }
-        
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
     }
-        
+
     public class SendButtonListener implements ActionListener {
+
         public void actionPerformed(ActionEvent ev) {
             try {
                 Message msg = new Message(outgoing.getText(), null, "mainroom");
                 writer.writeObject(msg);
                 writer.flush();
-            } catch (Exception ex)  { ex.printStackTrace(); }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
             outgoing.setText("");
             outgoing.requestFocus();
         }
     }
-    
+
     public class SetNameButtonListener implements ActionListener {
+
         public void actionPerformed(ActionEvent ev) {
             try {
                 Message msg = new Message(setName.getText(), null, "setName");
                 writer.writeObject(msg);
                 writer.flush();
-            } catch (Exception ex)  { ex.printStackTrace(); }
-        }  
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
-    
+
     public class IncomingReader implements Runnable {
+
         @Override
         public void run() {
             Message message;
@@ -110,10 +138,21 @@ public class ChatClient {
                 while ((message = (Message) reader.readObject()) != null) {
                     if (message.getDestination().equals("mainroom") || message.getDestination().equals("all")) {
                         incoming.append(message.getUser() + ": " + message.getContent() + "\n");
+                    } else if (message.getDestination().equals("userList")) {
+                        //update list of users
+                        String delims = "[,]";
+                        String[] tokens = message.getContent().split(delims);
+
+                        usersModel.clear();
+                        for (String user : tokens) {
+                            usersModel.addElement(user);
+                        }
                     }
                 }
-            } catch (Exception ex)  { ex.printStackTrace(); }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
-    
+
 }
--- Chat/src/chatserver/ChatServer.java
+++ Chat/src/chatserver/ChatServer.java
@@ -17,6 +17,7 @@ import java.util.*;
 public class ChatServer {
 
     Vector<User> users = new Vector<>();
+    List serverMessages = Arrays.asList("userList", "test");
 
     public class ClientHandler implements Runnable {
 
@@ -47,6 +48,7 @@ public class ChatServer {
             } catch (SocketException ex) { //handles disconnected users
                 broadcastMessage(new Message(users.get(userIndex).getUsername() 
                         + " disconnected: " + ex.getMessage(), "Server", "mainroom"));
+                updateUserList();
                 users.set(userIndex, null); //set disconnected users in array as null
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -70,14 +72,34 @@ public class ChatServer {
                 //set up client input and output streams in new user objects
                 ObjectOutputStream writer = new ObjectOutputStream(clientSocket.getOutputStream());
                 ObjectInputStream reader = new ObjectInputStream(clientSocket.getInputStream());
-                User newUser = new User("user" + (users.size() + 1), writer, reader, clientSocket);
+                User newUser = new User("user", writer, reader, clientSocket);
                 //set default name with user index
-                users.add(newUser);
                 
+                //check for any empty slots in vector and if available,
+                //use them instead of creating a new one
+                boolean isCreated = false;
+                for (int i = 0; i < users.size(); i++) {
+                    if (users.get(i) == null) {
+                        //set default username #
+                        newUser.setUsername(newUser.getUsername() + (i + 1));
+                        users.set(i, newUser);
+                        isCreated = true;
+                        break;
+                    }
+                }
+                if (!isCreated) {
+                    //set default username #
+                    newUser.setUsername(newUser.getUsername() + (users.size() + 1));
+                    users.add(newUser);
+                }                
 
-                Thread t = new Thread(new ClientHandler(users.indexOf(newUser)));
+                int userIndex = users.indexOf(newUser);
+                Thread t = new Thread(new ClientHandler(userIndex));
                 t.start();
-                System.out.println("Got a connection.");
+                //alert users of join
+                broadcastMessage(new Message(users.get(userIndex).getUsername() 
+                        + " has joined the room.", "Server", "mainroom"));
+                updateUserList();
             }
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -87,7 +109,10 @@ public class ChatServer {
     public synchronized void broadcastMessage(Message message) {
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i) != null) {
-                if (users.get(i).rooms.contains(message.getDestination())) {
+                //check if users are in room that message is being sent to
+                //also check that it is not a server message
+                if (users.get(i).rooms.contains(message.getDestination()) || 
+                        serverMessages.contains(message.getDestination())) {
                     try {
                         users.get(i).outputStream.writeObject(message);
                         users.get(i).outputStream.flush();
@@ -111,15 +136,33 @@ public class ChatServer {
     }
 
     public synchronized void setName(int userIndex, String name) {
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i) != null) {
-                if (users.get(i).getUsername().equals(name) && i != userIndex) {
-                    sendMessage(userIndex, new Message("Name " + name + " is already taken.",
-                            "Server", "all"));
-                    return;
+        //name cannot be empty
+        if (!name.equals("") && !name.equals(null)) {
+            //make sure name is not already taken
+            for (int i = 0; i < users.size(); i++) {
+                if (users.get(i) != null) {
+                    if (users.get(i).getUsername().equals(name) && i != userIndex) {
+                        sendMessage(userIndex, new Message("Name " + name + " is already taken.",
+                                "Server", "all"));
+                        return;
+                    }
                 }
             }
+            users.get(userIndex).setUsername(name);
+            updateUserList();
+        } else {
+            sendMessage(userIndex, new Message("Name cannot be empty.", "Server", "all"));
         }
-        users.get(userIndex).setUsername(name);
+    }
+    
+    public synchronized void updateUserList() {
+        //sends list of users to client for user list panel
+        String userList = "";
+        for (User user: users) {
+            if (user != null) {
+                userList += user.getUsername() + ",";
+            }
+        }
+        broadcastMessage(new Message(userList, "Server", "userList"));
     }
 }
